30ac003a411ebbecb02c69d1bb5a0d5b3811e170
BrokenTek <cwpribble@my.waketech.edu>
2023-04-26 05:29:23 -0400

    staged_push new feature_s2t

---
+++ src/speech_parser.js
@@ -0,0 +1,42 @@
+// // new speech recognition object
+// var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+// var recognition = new SpeechRecognition();
+
+// // This runs when the speech recognition service starts
+// recognition.onstart = function() {
+//     console.log("We are listening. Try speaking into the microphone.");
+// };
+
+// recognition.onspeechend = function() {
+//     // when user is done speaking
+//     recognition.stop();
+// }
+
+// // This runs when the speech recognition service returns result
+// recognition.onresult = function(event) {
+//     var transcript = event.results[0][0].transcript;
+//     var confidence = event.results[0][0].confidence;
+//     return transcript, confidence;
+// };
+
+// // start recognition
+// recognition.start();
+
+// // Shows the output by appending the transcript to the output
+// function showOutput(message) {
+//     var output = document.getElementById("output");
+//     output.innerHTML = message;
+// }
+
+// btn1 = document.getElementById("start_button");
+
+// btn1.onmouseover = showOutput(transcript);
+
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+recognition.onresult = function(event) {
+  if (event.results.length > 0) {
+    q.value = event.results[0][0].transcript;
+    q.form.submit();
+  }
+}
